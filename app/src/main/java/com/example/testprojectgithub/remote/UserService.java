879faf996a150de9e5cfff13352d10191f369e00
niklutfil <06dtk16f1007@gmail.com>
2023-06-20 22:54:02 +0800

    create class error, errorResponse, user, remote and model package

---
+++ app/src/main/java/com/example/testprojectgithub/remote/UserService.java
@@ -0,0 +1,12 @@
+package com.example.testprojectgithub.remote;
+import com.example.testprojectgithub.model.User;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface UserService
+{
+
+}
