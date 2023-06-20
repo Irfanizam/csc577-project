eca7b4828f78467935b6271d6aa69430b847716b
NikIrfann <irfann6719@gmail.com>
2023-06-21 00:44:56 +0800

    Update for REST retrieve

--- app/src/main/java/com/example/testprojectgithub/remote/ApiUtils.java
+++ app/src/main/java/com/example/testprojectgithub/remote/ApiUtils.java
@@ -3,10 +3,16 @@ package com.example.testprojectgithub.remote;
 
 public class ApiUtils
 {
-    public static final String BASE_URL = ;
+    public static final String BASE_URL = "https://https://csc577-nik.000webhostapp.com";
 
-    public static UserService.getUserService()
+    public static UserService getUserService()
     {
         return RetrofitClient.getClient(BASE_URL).create(UserService.class);
+    }
+
+    //return GameService instance
+    public static GameService getGameService()
+    {
+        return RetrofitClient.getClient(BASE_URL).create(GameService.class);
     }
 }
