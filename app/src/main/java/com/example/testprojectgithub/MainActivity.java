2fbc9b8e305f12310abbb0053729204956c61b87
NikIrfann <irfann6719@gmail.com>
2023-06-21 02:29:08 +0800

    Test

--- app/src/main/java/com/example/testprojectgithub/MainActivity.java
+++ app/src/main/java/com/example/testprojectgithub/MainActivity.java
@@ -2,24 +2,75 @@ package com.example.testprojectgithub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.testprojectgithub.model.SharedPrefManager;
+import com.example.testprojectgithub.model.User;
 
 public class MainActivity extends AppCompatActivity {
 
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView tvHelloWorld = findViewById(R.id.tvHelloWorld);
-        tvHelloWorld.setText("Testingttt");
+//        TextView tvHelloWorld = findViewById(R.id.tvHelloWorld);
+//        tvHelloWorld.setText("Hi");
+
+
+        context = this;
+
+        // get user info from SharedPreferences
+        User user = SharedPrefManager.getInstance(getApplicationContext()).getUser();
+
+        // assign action to logout button
+        Button btnLogout = findViewById(R.id.btnLogout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // clear the shared preferences
+                SharedPrefManager.getInstance(getApplicationContext()).logout();
+
+                // display message
+                Toast.makeText(getApplicationContext(),
+                        "You have successfully logged out.",
+                        Toast.LENGTH_LONG).show();
+
+                // forward to LoginActivity
+                finish();
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+
+            }
+        });
+
+        // assign action to Game List button
+        Button btnGameList = findViewById(R.id.btnGameList);
+        btnGameList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // forward user to BookListActivity
+                Intent intent = new Intent(context, GameListActivity.class);
+                startActivity(intent);
+            }
+        });
     }
-    public void doLogut(View view)
-    {
-        SharedPrefManager.getInstance(getApplicationContext().logout());
-        Toast.k
+
+    public void doLogout(View view) {
+    // clear the shared preferences
+        SharedPrefManager.getInstance(getApplicationContext()).logout();
+    // display message
+        Toast.makeText(getApplicationContext(),
+                "You have successfully logged out.",
+                Toast.LENGTH_LONG).show();
+    // forward to LoginActivity
+        finish();
+        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
     }
 }
