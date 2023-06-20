3c6247f90e38f3300a6331763f19e87399e97e9b
Irfanizam <irfanizam34@gmail.com>
2023-06-20 22:39:54 +0800

    add reference hello world on main

--- app/src/main/java/com/example/testprojectgithub/MainActivity.java
+++ app/src/main/java/com/example/testprojectgithub/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView tvHelloWorld = findViewById(R.id.tvHelloWorld);
-        tvHelloWorld.setText("Assalamualaikum");
+        tvHelloWorld.setText("Testingttt");
     }
 }
